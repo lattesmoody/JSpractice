68af4f6887781aff7375222f350782495d6a01e3
lattesmoody <lattesmoody@gmail.com>
2022-08-10 00:00:37 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// console.log(123);
+
+// variable(변수): 값을 저장하거나 유지하는 역할
+// variable을 만들기 위해서는 1. const 사용 2. let 사용
+// const란 바뀌지 않는 값, 계속 유지된다.
+// let: 값 바뀔 수 있음.
+const a = 5;
+const b = 2;
+
+let myName = "BESTCHOCO";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a /b);
+
+console.log("hello " + myName);
+
+myName = "SHIM"
+console.log("new name " + myName);
+
+// const 쓰는 이유는 variable이 절대 변하지 않도록 해야할 때가 있다.
+
+// const, let을 보고 코드 규칙을 대략적으로 파악 가능.
+// 예) myName은 어디선가 값이 새로 업데이트 될 수 있음.
