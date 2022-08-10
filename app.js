afca9e0894978b13c8da101468cd5a4a8c5ed32b
lattesmoody <lattesmoody@gmail.com>
2022-08-10 13:58:58 +0900

    #2.5 Arrays

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-// Booleans
+// 데이터 정리
+// array란 하나의 variable 안에 데이터의 list를 가지는 것.
 
-// true/false는 따로 따옴표를 붙이지 않는다. (파이썬은 True, False)
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-// null :  그 변수에 아무 값이 존재하지 않는다. (파이썬은 None)
-// 즉, variable 안에 어떤 값이 없다는 것을 확실히 하기 위해 쓰는 것.
-const choco = null; // choco라는 변수에 아무 것도 없는 상태로 채워 넣음. 
+console.log(daysOfWeek[0]);
 
-let something; // console.log 결과: undefined (메모리 공간에 존재, 값은 없음)
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato", "pizza"];
+
+toBuy.push("kimbab");
+
+console.log(toBuy);
