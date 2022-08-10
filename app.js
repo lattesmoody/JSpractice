0812628bd9419e62036e97c4cc49a5db459f2cdf
lattesmoody <lattesmoody@gmail.com>
2022-08-10 16:11:42 +0900

    #2.10 Recap II

--- app.js
+++ app.js
@@ -1,35 +1,25 @@
-// #2.7 Funcions
-
-// function: 반복해서 사용할 수 있는 코드 조각.
-// 코드를 캡슐화하여 실행을 여러 번 할 수 있게 해준다.
-
-function sayHello(nameOfPerson, age) {
-  console.log("hello! my name is ", nameOfPerson, " and I'm ", age);
+// #2.10 Recap II
+function plus(potato, salad){
+    console.log(potato + salad);
 }
 
-sayHello("a", 10);
-sayHello("b", 23);
-sayHello("c", 32);
+//plus(5,10);
 
-// argument: function을 실행하는 동안 어떤 정보를 function에게 보낼 수 있는 방법
-
-function plus(a, b) {
-  console.log(a + b);
+const calculator = {
+    add: function(a,b){
+        console.log(a+b);
+    },
+    minus: function(a,b){
+        console.log(a-b);
+    },
+    divide: function(a,b){
+        console.log(a/b);
+    },
+    pow: function(a,b){
+        console.log(a**b);
+    },
 }
-
-function divide(a, b) {
-  console.log(a / b);
-}
-plus(8, 60);
-divide (98,20);
-
-// NaN : Not a Number
-
-const player = {
-    name: "choco",
-    sayHello2: function(personName){
-        console.log("hello,",personName, "nice to meet you");
-    }
-}
-console.log(player.name);
-player.sayHello2("chocoya");
+calculator.add(5,2);
+calculator.minus(5,2);
+calculator.divide(5,2);
+calculator.pow(5,2);
