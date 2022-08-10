bb5ab070f0456a808719b720d885ea5d63c1a5a7
lattesmoody <lattesmoody@gmail.com>
2022-08-10 21:50:24 +0900

    #3.1

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-// #2.13 Conditionals (조건문)
+const title = document.getElementById("title") // HTML에서 id를 통해 element를 찾아준다.
 
-const age = parseInt(prompt("How old are you?"));
+//console.dir(title);
 
-console.log(isNaN(age)); // isNaN은 boolean을 반환한다.
+title.innerText = "Got you!";
 
-if (isNaN(age) || age < 0) {
-  // condition == true
-  console.log("올바른 값을 입력해주세요.");
-} else if (age < 18) {
-  // condition == false
-  //console.log("Thank you for writing your age.");
-  console.log("You are too young.");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink");
-} else if (age > 50 && age <= 80) {
-  console.log("for care Health");
-} else if (age > 80) {
-  console.log("Unbelievable");
-}
+console.log(title.id);
+console.log(title.className);
