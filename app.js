e697c6510bd9717bd40480a80d46267b8c8ed9d7
lattesmoody <lattesmoody@gmail.com>
2022-08-10 17:42:16 +0900

    // #2.13 Conditionals

--- app.js
+++ app.js
@@ -1,33 +1,20 @@
-// #2.11 Returns
+// #2.13 Conditionals (조건문)
 
-const age = 96;
-function calculateKrAge(ageOfForeigner){
-    return ageOfForeigner + 2;
+const age = parseInt(prompt("How old are you?"));
+
+console.log(isNaN(age)); // isNaN은 boolean을 반환한다.
+
+if (isNaN(age) || age < 0) {
+  // condition == true
+  console.log("올바른 값을 입력해주세요.");
+} else if (age < 18) {
+  // condition == false
+  //console.log("Thank you for writing your age.");
+  console.log("You are too young.");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink");
+} else if (age > 50 && age <= 80) {
+  console.log("for care Health");
+} else if (age > 80) {
+  console.log("Unbelievable");
 }
-
-const KrAge = calculateKrAge(age);
-
-console.log(KrAge);
-
-const calculator = {
-    plus: function(a,b){
-        return a+b;
-    },
-    minus: function(a,b){
-        return a-b;
-    },
-    divide: function(a,b){
-        return a/b;
-    },
-    pow: function(a,b){
-        return a**b;
-    }
-}
-
-const plusResult = calculator.plus(2,3);
-const minusResult = calculator.minus(plusResult, 10);
-
-//console.log(plusResult);
-//console.log(minusResult);
-
-// 변수에 값이 저장되어 있기에, function이 무언가를 return 했기에 각 변수에 접근하여 값을 알 수 있다.
