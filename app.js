a88bb0048d11e69bfa45d9e4f9e5e80b6e2a5441
lattesmoody <lattesmoody@gmail.com>
2022-08-10 16:28:01 +0900

    #2.11 Returns

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-// #2.10 Recap II
-function plus(potato, salad){
-    console.log(potato + salad);
+// #2.11 Returns
+
+const age = 96;
+function calculateKrAge(ageOfForeigner){
+    return ageOfForeigner + 2;
 }
 
-//plus(5,10);
+const KrAge = calculateKrAge(age);
+
+console.log(KrAge);
 
 const calculator = {
-    add: function(a,b){
-        console.log(a+b);
+    plus: function(a,b){
+        return a+b;
     },
     minus: function(a,b){
-        console.log(a-b);
+        return a-b;
     },
     divide: function(a,b){
-        console.log(a/b);
+        return a/b;
     },
     pow: function(a,b){
-        console.log(a**b);
-    },
+        return a**b;
+    }
 }
-calculator.add(5,2);
-calculator.minus(5,2);
-calculator.divide(5,2);
-calculator.pow(5,2);
+
+const plusResult = calculator.plus(2,3);
+const minusResult = calculator.minus(plusResult, 10);
+
+//console.log(plusResult);
+//console.log(minusResult);
+
+// 변수에 값이 저장되어 있기에, function이 무언가를 return 했기에 각 변수에 접근하여 값을 알 수 있다.
