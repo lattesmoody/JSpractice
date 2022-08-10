77ba29b8b3f72d2b34ab24fb3b05932dbc8c9a39
lattesmoody <lattesmoody@gmail.com>
2022-08-10 12:59:50 +0900

    # 2.3 const and let

--- app.js
+++ app.js
@@ -22,4 +22,13 @@ console.log("new name " + myName);
 
 // const, let을 보고 코드 규칙을 대략적으로 파악 가능.
 // 예) myName은 어디선가 값이 새로 업데이트 될 수 있음.
-//5:22  https://www.youtube.com/watch?v=Wm1Fj1tLkFs
+
+// 보통 const를 기본적으로 사용.
+// variable 업데이트 필요 시 let 사용.
+
+// 예전엔 let, const 이전에 var을 사용. 
+// var의 경우 실수로 업데이트 하지 말아야 할 값을 업데이트 했을 때 자바스크립트는 잘못되었다고 알려주지 않음.
+// var은 아무런 규칙을 가지고 있지 않다. (왠만하면 쓰지 말기.)
+
+
+
