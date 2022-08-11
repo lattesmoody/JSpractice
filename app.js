fb07da57b87f1bc8a2d9897b29d535e2731de0f0
lattesmoody <lattesmoody@gmail.com>
2022-08-11 16:35:32 +0900

    # 3.7 CSS Part 2

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
-  if (currentColor === "blue"){
-    newColor = "tomato";
+  //h1.className = "active"; //className: getter/setter
+  //console.log(h1.className);
+  const clickedClass = "clicked";
+  if(h1.className === clickedClass){
+    h1.className = "";
   }else{
-    newColor = "blue"
+    h1.className = clickedClass;
   }
-  h1.style.color = newColor;
 }
-
+// raw value는 지양할 것.
 h1.addEventListener("click",handleTitleClick);
 
-// #3.6 CSS in Javascript
-// step 1. find element.
-// step 2. event listen.
-// step 3. react event
+// #3.7 CSS in Javascript
+// JavaScript로 모든 class name을 변경하지 않아야 함.
+// 단지 clicked class를 변경하고 싶다.
