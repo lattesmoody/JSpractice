8bdf7c3a2ad75f23e9020a522198e18d6411e056
lattesmoody <lattesmoody@gmail.com>
2022-08-11 14:23:51 +0900

    #3.5 More Events

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
-// app.js가 있기 때문에 javascript를 통해 HTML의 내용르 가져올 수 있다.
+const h1 = document.querySelector("div.hello:first-child h1");
+console.dir(h1);
 
-// document가 HTML이 app.js를 load하기 때문에 존재하는 것.
-// 그 다음 browser가 우리가 document에 접근 할 수 있게 해준다.
-
-// querySelector는 CSS Selector를 전달할 수 있다.
-
-const title = document.querySelector("div.hello:first-child h1");
-
-
-//title.style.color = "blue"
-
-// JavaScript에서 대부분 작업할 일은 event를 listen.
-
-function handleTitleClick(){
-    title.style.color = "blue"
-    console.log("title was clicked!");
+function handleh1Click() {
+  h1.style.color = "blue";
+  console.log("h1 was clicked!");
 }
 
-title.addEventListener("click",handleTitleClick)
-// listen 하고 싶은 event, event 발생 시 호출할 function 전달.
-// 두 번째 인자의 경우 괄호 포함되서는 안 된다.
-console.dir(title);
-// property 이름 앞에 on이 붙어있다면, 그게 바로 event listener
-// 이벤트 사용 시 on을 제외하고 사용.
-
-function handleMouseEnter(){
-    //console.log("mouse is here!");
-    title.innerText = "Mouse is here!";
+function handleMouseEnter() {
+  h1.innerText = "Mouse is here!";
 }
 
-function handleMouseLeave(){
-    title.innerText = "Mouse is gone!";
+function handleMouseLeave() {
+  h1.innerText = "Mouse is gone!";
 }
-title.addEventListener("mouseenter",handleMouseEnter);
-title.addEventListener("mouseleave",handleMouseLeave);
+//h1.addEventListener("click", handleh1Click);
+h1.onclick = handleh1Click;
+//h1.addEventListener("mouseenter", handleMouseEnter);
+h1.onmouseenter = handleMouseEnter;
+h1.addEventListener("mouseleave", handleMouseLeave);
+// #3.5 More Events
+
+// addEventListener를 더 선호하는 이유
+// => removeEventListener를 통해 event listener 제거 가능.
+
+function handleWindowResize(){ // 화면크기가 바뀌면
+    document.body.style.backgroundColor = "skyblue";
+}
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+
+function handleWindowOffline(){
+    alert("OFFLINE internet");
+}
+
+function handleWindowOnline(){
+    alert("ONLINE internet");
+}
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+// clipboard events: 유저가 copy 행위를 했을 때
+window.addEventListener("offline",handleWindowOffline);
+window.addEventListener("online",handleWindowOnline);
